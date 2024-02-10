20f0d6870c2678ff60b4897836b0a2b93ca68e56
DongDong <qltkd5959@gmail.com>
2024-02-10 21:35:52 +0900

    (feat) - add to express route middleware

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 const express = require("express");
 const app = express();
+const router = express.Router();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+router
+  .route("/contact/:id")
+  .get((req, res) => {
+    res.send(`Select ${req.params.id}`);
+  })
+  .post((req, res) => {
+    res.send(`Create ${req.params.id}`);
+  });
+
+// app에 router 미들웨어를 사용했음을 알린다.
+app.use(router);
 
 app.listen(3000, () => {
   console.log("server running ... ");
-});
-
-app.get("/contact", (req, res) => {
-  res.send(`Select All`);
-});
-
-app.get("/contact/:id", (req, res) => {
-  res.send(`Select ${req.params.id}`);
-});
-
-app.post("/contact/:id", (req, res) => {
-  res.send(`Create ${req.params.id}`);
-});
-
-app.put("/contact/:id", (req, res) => {
-  res.send(`Update ${req.params.id}`);
-});
-
-app.delete("/contact/:id", (req, res) => {
-  res.send(`Delete ${req.params.id}`);
 });
